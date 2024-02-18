cb76a6a69ceb1165ac75b4679fa817a805c665ca
ShoheiTakei <st.jwd196@gmail.com>
2024-02-18 22:46:50 +0900

    描画最適化 add Consumer

--- lib/main.dart
+++ lib/main.dart
@@ -23,37 +23,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends ConsumerWidget {
-  const MyHomePage({super.key, });
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
+    // countUpの値だけ再描画すればいいが、全体が再描画される
+    print('MyHomePage rebuild');
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          ref.watch(titleProvider),
-        ),
-      ),
+      appBar: AppBar(title: Consumer(
+        builder: (BuildContext context, WidgetRef ref, Widget? child) {
+          return Text(ref.watch(titleProvider));
+        },
+      )),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Consumer(
+              builder: (context, ref, child) {
+                return Text(
+                  ref.watch(countUpDetail),
+                );
+              },
             ),
-            Text(
-              ref.watch(countProvider).toString(),
-              style: Theme.of(context).textTheme.headlineMedium,
+            Consumer(
+              builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                return Text(
+                  ref.watch(countProvider).toString(),
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(countProvider.notifier).state++;
+      floatingActionButton: Consumer(
+        builder: (BuildContext context, WidgetRef ref, Widget? child) {
+          print('floatingActionButton rebuild');
+          return FloatingActionButton(
+            onPressed: () {
+              ref.watch(countProvider.notifier).state++;
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          );
         },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
